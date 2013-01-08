9fb07e4452c194fcc09866a8475124b79cc7b4d0
Ophir Cohen <ophchu@gmail.com>
2013-01-08 16:47:56 +0200

    Add

--- ruby/goliath/hello.rb
+++ ruby/goliath/hello.rb
@@ -4,24 +4,25 @@ class HelloGoliath < Goliath::API
     env.logger.info "Connection closed."
   end
 
-  #def response (env)
-  #  [200, {}, "Hello Goliath!"]
-  #end
-
-  def response(env)
-    i = 0
-    pt = EM.add_periodic_timer(1) do
-      env.stream_send("#{i}\n")
-      i += 1
-    end
-
-    EM.add_timer(10) do
-      pt.cancel
-
-      env.stream_send("!! BOOM !!\n")
-      env.stream_close
-    end
-
-    streaming_response(202, {'X-Stream' => 'Goliath'})
+  def response (env)
+    #[200, {}, "Hello Goliath!"]
+    [200, {}, params.to_s]
   end
+
+  #def response(env)
+  #  i = 0
+  #  pt = EM.add_periodic_timer(1) do
+  #    env.stream_send("#{i}\n")
+  #    i += 1
+  #  end
+  #
+  #  EM.add_timer(10) do
+  #    pt.cancel
+  #
+  #    env.stream_send("!! BOOM !!\n")
+  #    env.stream_close
+  #  end
+  #
+  #  streaming_response(202, {'X-Stream' => 'Goliath'})
+  #end
 end
