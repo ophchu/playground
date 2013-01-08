0315ac578830b68463b0e1800d8d1684da793c34
Ophir Cohen <ophchu@gmail.com>
2013-01-08 13:30:40 +0200

    Add only for branch

--- ruby/goliath/hello.rb
+++ ruby/goliath/hello.rb
@@ -1,6 +1,27 @@
 require 'goliath'
 class HelloGoliath < Goliath::API
-  def response (env)
-    [200, {}, "Hello Goliath!"]
+  def on_close(env)
+    env.logger.info "Connection closed."
+  end
+
+  #def response (env)
+  #  [200, {}, "Hello Goliath!"]
+  #end
+
+  def response(env)
+    i = 0
+    pt = EM.add_periodic_timer(1) do
+      env.stream_send("#{i}\n")
+      i += 1
+    end
+
+    EM.add_timer(10) do
+      pt.cancel
+
+      env.stream_send("!! BOOM !!\n")
+      env.stream_close
+    end
+
+    streaming_response(202, {'X-Stream' => 'Goliath'})
   end
 end
